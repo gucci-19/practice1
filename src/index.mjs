24608e0dc7ab126156f207abb652195259f7093f
gucci-19 <kitaguchi19ryota@gmail.com>
2024-06-26 04:14:16 +0000

    セクション5,36　エラー（console表示）

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,13 @@
 import "./styles.css";
 
 const onClickAdd = () => {
-  alert();
+  // テキストボックスの値をテキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // li生成
+  const li = document.createElement("li");
+  console.log(li);
 };
 
 document.getElementById("add-button").addEventListener("click", onClickAdd);
