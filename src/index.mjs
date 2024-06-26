7abac819f04df9c4e7649c897b2962750502f0fb
gucci-19 <kitaguchi19ryota@gmail.com>
2024-06-26 05:33:23 +0000

    セクション5,36 完了

--- src/index.mjs
+++ src/index.mjs
@@ -17,9 +17,12 @@ const onClickAdd = () => {
   p.className = "todo-item";
   p.innerText = inputText;
 
+  //liタグの子要素に各要素を設定
   div.appendChild(p);
+  li.appendChild(div);
 
-  console.log(div);
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
 };
 
 document.getElementById("add-button").addEventListener("click", onClickAdd);
