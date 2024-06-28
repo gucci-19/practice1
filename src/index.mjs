2ec392e84f11fca54284c11afa3f206c6932ef63
gucci-19 <kitaguchi19ryota@gmail.com>
2024-06-28 07:33:56 +0000

    セクション5,40 完了

--- src/index.mjs
+++ src/index.mjs
@@ -5,6 +5,12 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  //未完了リストに追加
+  createIncompleteTodo(inputText);
+};
+
+// 渡された引数を基に未完了のTODOを作成する関数
+const createIncompleteTodo = (todo) => {
   // li生成
   const li = document.createElement("li");
 
@@ -15,13 +21,29 @@ const onClickAdd = () => {
   // p生成
   const p = document.createElement("p");
   p.className = "todo-item";
-  p.innerText = inputText;
+  p.innerText = todo;
 
   // button(完了)タグ生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    // 押された完了ボタンの親にあるliタグは以下の完了ボタンと削除ボタンを削除
+    const moveTarget = completeButton.closest("li");
+    completeButton.nextElementSibling.remove();
+    completeButton.remove();
+    // 戻すボタンを生成してdivタグ配下に設定
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // TODOの内容を取得し、未完了リストに追加
+      const todoText = backButton.previousElementSibling.innerText;
+      createIncompleteTodo(todoText);
+      // 押された戻すボタンの親にあるliタグを削除
+      backButton.closest("li").remove();
+    });
+    moveTarget.firstElementChild.appendChild(backButton);
+    // 完了リストに移動
+    document.getElementById("complete-list").appendChild(moveTarget);
   });
 
   // button(削除)タグ生成
